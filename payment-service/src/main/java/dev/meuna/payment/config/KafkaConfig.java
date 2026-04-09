52b21a86912951f8f386b33dc91617ead54b83fd
Meuna <ahmeddilda@gmail.com>
2026-04-09 08:18:18 +0500

    feat(config): add per-service Kafka config

---
+++ payment-service/src/main/java/dev/meuna/payment/config/KafkaConfig.java
@@ -0,0 +1,33 @@
+package dev.meuna.payment.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class KafkaConfig {
+	
+	@Value(value = "${spring.kafka.bootstrap-servers}")
+	private String bootstrapAddress;
+	
+	@Bean
+	public KafkaAdmin kafkaAdmin() {
+		log.info("Init kafka admin");
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		return new KafkaAdmin(configs);
+	}
+	
+	@Bean
+	public NewTopic topic1() {
+		return new NewTopic("test-from-payment", 1, (short) 1);
+	}
+}
