2d340ffc6bc946876ee0c4a6f0544d9317992a20
Meuna <ahmeddilda@gmail.com>
2026-04-24 07:51:42 +0500

    feat(kafka): add Kafka topics

--- assessment-service/src/main/java/dev/meuna/assessment/config/KafkaConfig.java
+++ assessment-service/src/main/java/dev/meuna/assessment/config/KafkaConfig.java
@@ -1,21 +1,14 @@
 package dev.meuna.assessment.config;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
 	
 	@Bean
-	public NewTopic claimAssessTopic1() {
+	public NewTopic claimAssessTopic() {
 		return new NewTopic("claim-assess", 1, (short) 1);
 	}
 	
